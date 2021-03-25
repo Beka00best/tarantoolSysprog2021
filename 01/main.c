574ebd1333ad0d6edd5544795b09303d133f6f17
Beka00best <bukhatovb@mail.ru>
2021-03-25 22:34:54 +0600

    1

--- 01/main.c
+++ 01/main.c
@@ -3,263 +3,233 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <sys/mman.h>
-#include <limits.h>
-#include <time.h>                                             
+#include <sys/time.h>
+#include <unistd.h>
+#include <time.h>
 
-
-static ucontext_t *uctx_func1 = NULL, uctx_main;             
-static ucontext_t uctx_start;
-
+#define STACK_SIZE 1024*1024*4
+#define goto_unfinished() \
+	do{ \
+		do{ \
+			c_i=(c_i+1)%c_s; \
+		}while(coroutine_finished[c_i]); \
+	}while(0)
+#define switch_coro_fast() \
+	do { \
+		save = c_i; \
+		goto_unfinished(); \
+		if(c_i!=save){ \
+			switch_cont_n[c_i]++; \
+			swapcontext(&contexts[save], &contexts[c_i]); \
+			c_i = save; \
+		} \
+}while(0)
+#define switch_coro() \
+	do { \
+		clock_gettime(CLOCK_REALTIME, &t_end); \
+		int delta = (t_end.tv_sec-t_start.tv_sec)*1000000000LLU + t_end.tv_nsec-t_start.tv_nsec; \
+		if (delta>1000*(T/c_s)){ \
+			contexts_times[c_i] += delta; \
+			save = c_i; \
+			goto_unfinished(); \
+			if(c_i!=save){ \
+				switch_cont_n[c_i]++; \
+				swapcontext(&contexts[save], &contexts[c_i]); \
+				c_i = save; \
+			} \
+			clock_gettime(CLOCK_REALTIME, &t_start); \
+		} \
+}while(0)
+#define finish_others() \
+	do{ \
+		clock_gettime(CLOCK_REALTIME, &t_end); \
+		contexts_times[c_i] += (t_end.tv_sec-t_start.tv_sec)*1000000000LLU + t_end.tv_nsec-t_start.tv_nsec; \
+		if(len == orgn_len){ \
+			c_ret_n++; \
+			coroutine_finished[c_i]=1; \
+			if(c_ret_n==c_s){ \
+				swapcontext(&contexts[c_i], &main_context); \
+			} \
+			while(c_ret_n<c_s){ \
+				switch_coro_fast(); \
+			} \
+		} \
+		clock_gettime(CLOCK_REALTIME, &t_start); \
+	}while(0)
 #define handle_error(msg) \
-  do { perror(msg); exit(EXIT_FAILURE); } while (0)
+   do { perror(msg); exit(EXIT_FAILURE); } while (0)
+typedef struct arr{
+	int * ptr;
+	int len;
+	int pos;
+}arr;
+int switch_n=0;
+ucontext_t main_context;
+ucontext_t * contexts;
+unsigned long long *  contexts_times;
+unsigned long long *  switch_cont_n;
+int * coroutine_finished;
+int c_i=0; //current context id
+int c_s=0; //number of contexts
+int c_ret_n=0;
+double T=0;//target latency microsec
+int min(arr * list_of_arr, int size, int * ret);
+void merge(arr * list_of_arr, int size , FILE * fp);
 
-#define stack_size 1024*1024                                            
-int *point = NULL;
-double *times = NULL;
-
-static void *
-allocate_stack_sig()
-{
-	void *stack = malloc(stack_size);
-	stack_t ss;
-	ss.ss_sp = stack;
-	ss.ss_size = stack_size;
-	ss.ss_flags = 0;
-	sigaltstack(&ss, NULL);
-	return stack;
-}
-
-static void *
-allocate_stack_mmap()
-{
-	return mmap(NULL, stack_size, PROT_READ | PROT_WRITE | PROT_EXEC,
-	        MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-}
-
-static void *
-allocate_stack_mprot()                                                      
-{
-	void *stack = malloc(stack_size);
-	mprotect(stack, stack_size, PROT_READ | PROT_WRITE | PROT_EXEC);
-	return stack;
-}
-
-enum stack_type {
-	STACK_MMAP,
-	STACK_SIG,
-	STACK_MPROT
-};
-
-static void *
-allocate_stack(enum stack_type t)
-{
-	switch(t) {
-	case STACK_MMAP:
-		return allocate_stack_mmap();
-	case STACK_SIG:
-		return allocate_stack_sig();
-	case STACK_MPROT:
-		return allocate_stack_mprot();
+void sort(int * ptr, int len, int orgn_len) {
+	struct timespec t_start;
+	struct timespec t_end;
+	clock_gettime(CLOCK_REALTIME, &t_start);
+	int save=0;switch_coro();
+	int i=0, j=0;switch_coro();
+	int temp;switch_coro();
+	int pivot =  ptr[len / 2];switch_coro();
+	if (len < 2) {
+		finish_others();
+		return;
 	}
+	for (i=0, j=len-1; ;i++,j--) {switch_coro();
+		while (pivot>ptr[i]) {i++;switch_coro();}
+		while (ptr[j]>pivot) {j--;switch_coro();}
+		if (i>=j) break;switch_coro();
+		temp = ptr[i];switch_coro();
+		ptr[i] = ptr[j];switch_coro();
+		ptr[j] =temp;switch_coro();
+	}
+	sort(ptr, i, orgn_len);
+	sort(ptr+i, len-i, orgn_len);
+	clock_gettime(CLOCK_REALTIME, &t_start);
+	finish_others();
 }
 
-void merge(int *a, int n, int index, int size) {         
-    clock_t start = clock();
-    int begin[150], end[150], LEFT, RIGHT;
-    int i = 0;
-    begin[0] = 0;
-    end[0] = n;
-    while (i >= 0) {
-        LEFT = begin[i];
-        RIGHT = end[i];
-        if (LEFT + 1 < RIGHT--) {
-            int piv = a[LEFT];
-            while (LEFT < RIGHT) {
-                while (a[RIGHT] >= piv && LEFT < RIGHT)
-                    RIGHT--;
-                if (LEFT < RIGHT)
-                    a[LEFT++] = a[RIGHT];
-                while (a[LEFT] <= piv && LEFT < RIGHT)
-                    LEFT++;
-                if (LEFT < RIGHT)
-                    a[RIGHT--] = a[LEFT];
-            }
-            a[LEFT] = piv;
-            if (LEFT - begin[i] > end[i] - RIGHT) { 
-                begin[i + 1] = LEFT + 1;
-                end[i + 1] = end[i];
-                end[i++] = LEFT;
-            } else {
-                begin[i + 1] = begin[i];
-                end[i + 1] = LEFT;
-                begin[i++] = LEFT + 1;
-            }
-        } else {
-            i--;
-        }
-          
-      if (activeCoroutine(size, index) != -1){                             
-          if(swapcontext(&uctx_func1[index], &uctx_func1[nextCoroutine(size, index)]) == -1) {
-              handle_error("swapcontext");
-          }
-      }
-    }
-    clock_t stop = clock();
-    times[index] = (double)(stop - start) / (CLOCKS_PER_SEC);
-    point[index] = 1;
+static void *
+allocate_stack()
+{
+	void *stack = malloc(STACK_SIZE);
+	mprotect(stack, STACK_SIZE, PROT_READ | PROT_WRITE | PROT_EXEC);
+	return stack;
 }
 
-int activeCoroutine(int n, int index){                                  
-    for(int i = 0; i < n; i++){
-        if ((point[i] == 0) && (i != index)){
-            return i;
-        }
-    }
-    return -1;
+int main(int argc, char const *argv[]) {
+	FILE * fp;
+	int len=argc-2;
+	c_s=len;
+	int size=0;
+	int tmp=0;
+	double Elapsed;
+	struct timeval t_start;
+	struct timeval t_end;
+
+
+	if(argc<3){
+
+		printf("Usage :./f T filename ...\n");
+		return 0;
+	}
+	arr *list_of_arr=malloc(len*sizeof(arr));
+	contexts=malloc(len*sizeof(ucontext_t));
+	contexts_times=malloc(len*sizeof(unsigned long long));
+	switch_cont_n = malloc(len*sizeof(unsigned long long));
+	coroutine_finished = malloc(len*sizeof(int));
+	for(int i=0;i<len;i++){
+		contexts_times[i] = 0;
+		switch_cont_n[i] = 0;
+		coroutine_finished[i] =0;
+	}
+	T = atof(argv[1]);
+	for(int i=0;i<len;i++){
+		fp = fopen(argv[i+2], "r");
+		int l_i=0;
+		while(fscanf(fp, "%d", &tmp)==1){
+			l_i++;
+		}
+		size+=l_i;
+		list_of_arr[i].ptr = malloc(l_i*sizeof(int));
+		list_of_arr[i].len = l_i;
+		list_of_arr[i].pos = 0;
+		fclose(fp);
+	}
+	for(int i=0;i<len;i++){
+		fp = fopen(argv[i+2], "r");
+		int k=0;
+		while(fscanf(fp, "%d", &list_of_arr[i].ptr[k])==1){
+			k++;
+		}
+		fclose(fp);
+	}
+	void ** save_stacks= malloc(len*sizeof(void *));
+	for(int i=len-1;i>=0;i--){
+		if (getcontext(&contexts[i]) == -1)
+			handle_error("getcontext");
+		contexts[i].uc_stack.ss_sp=allocate_stack();
+		save_stacks[i] = contexts[i].uc_stack.ss_sp;
+		contexts[i].uc_stack.ss_size=STACK_SIZE;
+		if(i==len-1)contexts[i].uc_link =&main_context;
+		else contexts[i].uc_link =&contexts[i+1];
+		makecontext(&contexts[i], (void (*)(void))sort, 3, list_of_arr[i].ptr, list_of_arr[i].len, list_of_arr[i].len);
+	}
+	getcontext(&main_context);
+
+	int ch=0;
+	gettimeofday(&t_start, 0);
+	if(!ch){ ch=1;
+
+		if (swapcontext(&main_context, &contexts[c_i=0]) == -1)
+		handle_error("swapcontext");
+	}
+	gettimeofday(&t_end, 0);
+	for(int i=0;i<len;i++){
+		printf("coro%d time = %12llu, switch_n = %llu\n", i, contexts_times[i]/1000, switch_cont_n[i]);
+	}
+	fp = fopen("result.txt", "w");
+	merge(list_of_arr, len, fp);
+	fclose(fp);
+	for(int i=0;i<len;i++){
+		free(list_of_arr[i].ptr);
+		free(save_stacks[i]);
+	}
+	free(save_stacks);
+	free(list_of_arr);
+	free(contexts);
+	free(contexts_times);
+	free(switch_cont_n);
+	free(coroutine_finished);
+	printf("Elapsed =    %12lld\n", (t_end.tv_sec-t_start.tv_sec)*1000000LL + (t_end.tv_usec-t_start.tv_usec));
+	return 0;
 }
 
-int nextCoroutine(int n, int index){                            
-    int tmp = index % n;
-    for(int i = 0; i < 2 * n; i++){
-        if ((point[i % n] == 0) && (i % n != index) && ((i % n > tmp) || (i >= n))){
-            return i % n;
-        }
-    }
-    return -1;
-}
+// int min(arr * list_of_arr, int size, int * ret){
+// 	int min;
+// 	int k=0;
+// 	int s=0;
+// 	int i=0;
+// 	for(i =0; i<size;i++){
+// 		if(list_of_arr[i].pos<list_of_arr[i].len){
+// 			min = list_of_arr[i].ptr[list_of_arr[i].pos];
+// 			k=i;
+// 			break;
+// 		}
+// 	}
+// 	if(i==size){
+// 		return 0;
+// 	}
+// 	for(int i =k; i<size;i++){
+// 		if(list_of_arr[i].pos<list_of_arr[i].len){
+// 			if(min>=list_of_arr[i].ptr[list_of_arr[i].pos]){
+// 				min = list_of_arr[i].ptr[list_of_arr[i].pos];
+// 				s=i;
+// 			}
+// 		}
+// 	}
+// 	list_of_arr[s].pos++;
+// 	*ret = min;
+// 	return 1;
+// }
 
-int minIndex(int n, int *a) {              
-    int min = INT_MAX;
-    int index = -1;
-    for (int i = 0; i < n; i++){
-        if((a[i] < min) && (a[i]!=-1)){
-            min = a[i];
-            index = i;
-        }
-    }
-    return index;
-}
 
-void procCoroutine(int n){                                          
-    while(1){
-        if (activeCoroutine(n, -1) == -1){
-            swapcontext(&uctx_start, &uctx_main);
-            break;
-        }
-        else{
-            swapcontext(&uctx_start, &uctx_func1[activeCoroutine(n, -1)]);
-        }
-    }
-}
-
-void finalMerge(int **arr, int n, char *file, int *sizes){ 
-    int *p = (int *)malloc(n * sizeof(int));                                        
-    int *tmp = (int*)malloc(n * sizeof(int));                                       
-    for(int i = 0; i < n; i++){
-        p[i] = 0;
-    }
-    for (int i = 0; i < n; i++){
-        tmp[i] = arr[i][p[i]];
-    }
-    FILE *fd = fopen(file, "w");
-    int min;
-    int j = 0;
-    while (j < n){
-        min = minIndex(n, tmp);
-        fprintf(fd, "%d ", arr[min][p[min]]);
-        p[min]++;
-        if (p[min] == sizes[min]){
-            j++;
-            tmp[min] = -1;                                                           
-        }                                                                                
-        else{                                                                               
-            tmp[min] = arr[min][p[min]];                            
-        }
-    }
-    free(p);
-    free(tmp);
-    fclose(fd);
-}
-
-int main(int argc, char **argv) {
-    clock_t start = clock();
-    point = (unsigned int *)malloc(sizeof(unsigned int)*(argc-1));
-    times = (double *)malloc(sizeof(double)*(argc-1));  
-
-    for (int i = 0; i < argc-1; i++){
-        point[i] = 0;
-    }
-
-    int *sizes = NULL;
-    sizes = (int*)malloc(sizeof(int) * (argc-1));
-    FILE *fp = NULL;
-    int i = 0;
-    int size = 0;
-    int num;
-    int  **numbers;
-    numbers = (int **) malloc((argc-1) * sizeof(numbers));
-
-    for(int i = 0; i < argc - 1; i++){
-        numbers[i] = NULL;
-    }
-
-    uctx_func1 = (ucontext_t *)malloc((argc) * sizeof(ucontext_t));
-    for (int i = 0; i < argc-1; i++){                                                       
-        size = 0;
-        if ((fp = fopen(argv[i+1], "r")) < 0){
-            handle_error("fopen");
-        }; 
-        while (fscanf(fp, "%d", &num) == 1 ){
-            if ((numbers[i] = (int*)realloc(numbers[i], sizeof(int) * (size + 1)) ) == NULL){
-                perror("realloc");
-                exit(EXIT_FAILURE);
-            }
-            numbers[i][size++] = num;
-        }
-        sizes[i] = size;
-        
-    }
-    fclose(fp);
-
-    getcontext(&uctx_start);                                                       
-    uctx_start.uc_stack.ss_sp = allocate_stack_mprot();
-    uctx_start.uc_stack.ss_size = stack_size;
-    uctx_start.uc_link = &uctx_main;
-    makecontext(&uctx_start, procCoroutine, 1, (argc-1));
-
-    for(int i = 0; i < argc-1; i++){                                                
-        if(getcontext(&uctx_func1[i]) == -1){
-            handle_error("getcontext");
-        }
-        uctx_func1[i].uc_stack.ss_sp = allocate_stack_mprot();
-        uctx_func1[i].uc_stack.ss_size = stack_size;
-        uctx_func1[i].uc_link = &uctx_start;
-        makecontext(&uctx_func1[i], merge, 4, numbers[i], sizes[i], i, argc-1);
-    }
-
-    swapcontext(&uctx_main, &uctx_start);
-    
-    int size_of_last_one = 0;
-    for (int k = 0; k < argc-1; k++){
-        size_of_last_one = size_of_last_one + sizes[k];
-    }
-    
-    finalMerge(numbers, argc-1, "res.txt", sizes);                         
-    clock_t stop = clock();
-    double spent = (double)(stop - start)/(CLOCKS_PER_SEC);
-    printf("Spent time %f second\n", spent);
-    for(int i = 0; i < argc - 1; i++){
-        printf("#%d coroutine spent %f second\n", i, times[i]);
-    }
-    for(int i = 0; i < argc - 1; i++){
-        free(uctx_func1[i].uc_stack.ss_sp);
-        free(numbers[i]);
-    }
-
-    free(uctx_func1);
-    free(sizes);
-    free(point);
-    free(numbers);
-    free(times);
-    return 0;
-}
+// void merge(arr * list_of_arr, int size , FILE * fp){
+// 	int a;
+//   while(min(list_of_arr, size, &a)){
+// 		fprintf(fp, "%d\n", a);
+// 	}
+// }
